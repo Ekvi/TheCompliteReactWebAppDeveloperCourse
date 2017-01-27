30b47921c7c86402317230dd8eb84c880174de51
Dmitrii <dima_sh84@mail.ru>
2017-01-27 23:29:22 +0200

    routes

---
+++ 03-weather app/app/components/WeatherPage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherPage = React.createClass({
+    render: function () {
+        return (
+            <h3>Weather component</h3>
+        );
+    }
+});
+
+module.exports = WeatherPage;
