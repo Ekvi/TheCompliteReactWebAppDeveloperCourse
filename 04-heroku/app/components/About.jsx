87163b7f52cbdf3fe15ed69ee0ade4b015ca8bce
Dmitrii <dima_sh84@mail.ru>
2017-01-30 23:23:15 +0200

    fixed localhost

--- 04-heroku/app/components/About.jsx
+++ 04-heroku/app/components/About.jsx
@@ -1,17 +1,11 @@
 var React = require('react');
 
-/*var About = React.createClass({
-    render: function () {
-        return (
-            <h3>About component</h3>
-        );
-    }
-});*/
-
-//for stateless components we can use this syntax
 var About = (props) => {
     return (
-        <h3>About component</h3>
+        <div>
+            <h3>About</h3>
+            <p>Welcome to about page</p>
+        </div>
     );
 };
 
